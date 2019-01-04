7166d753d555e8ffb311499cd437798411d4d400
Marx <marx3073@163.com>
2019-01-04 22:59:21 +0800

    create project

---
+++ SWTQ/SWTQ/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SWTQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.txtContent = new System.Windows.Forms.RichTextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 478);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Visible = false;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(0, 0);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(800, 450);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.Text = "";
+            this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblCount.Location = new System.Drawing.Point(5, 454);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(95, 19);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "总字数：0";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(539, 453);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 3;
+            this.btnRandom.Text = "切换文章";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(626, 453);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "去除空格";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(713, 453);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "保存文本";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "每日一文抓取工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.RichTextBox txtContent;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SWTQ/SWTQ/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SWTQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("https://www.meiriyiwen.com");
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(web_DocumentCompleted);
+        }
+
+        void web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser web = (WebBrowser)sender;
+            foreach (HtmlElement ele in webBrowser.Document.GetElementsByTagName("div"))
+            {
+                var className = ele.GetAttribute("className");
+                if (className == "article_text")
+                {
+                    txtContent.Text = ele.InnerText;
+                }
+            }
+        }
+
+        private void txtContent_TextChanged(object sender, EventArgs e)
+        {
+            lblCount.Text = $"总字数：{txtContent.Text.Replace("\r", "").Replace("\n", "").Length}";
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("https://meiriyiwen.com/random");
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            txtContent.Text = txtContent.Text.Replace("\r", "").Replace("\n", "").Replace(" ","");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            //设置保存文件对话框的标题
+            sfd.Title = "请选择要保存的文件路径";
+            //初始化保存目录，默认桌面
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //设置保存文件的类型
+            sfd.Filter = "文本文件|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //获得保存文件的路径
+                string filePath = sfd.FileName;
+                //保存
+                using (var ws = new StreamWriter(filePath, false))
+                {
+                    ws.Write(txtContent.Text);
+                }
+                MessageBox.Show("保存成功！");
+            }
+        }
+    }
+}
